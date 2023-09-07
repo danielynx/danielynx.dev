933e3ec14045f643e0bc559c405d7dd2e3ad5076
Daniel B <danielynx@proton.me>
2023-09-07 10:08:28 -0400

    feat: :sparkles: add the footer content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,18 @@ import { PropsWithChildren } from 'react';
 import { Open_Sans as Sans } from 'next/font/google';
 import { Roboto_Serif as Serif } from 'next/font/google';
 
+import { SiNextdotjs as SiNextJs } from 'react-icons/si';
+import { SiTailwindcss as SiTailwind } from 'react-icons/si';
+import { SiReact } from 'react-icons/si';
+import { SiTypescript } from 'react-icons/si';
+
 import { LayoutProfileHeader } from '@/components/home/layout/ProfileHeader';
 import { LayoutProfileBody } from '@/components/home/layout/ProfileBody';
 import { ProfileIntersectionProvider } from '@/contexts/home/layout/ProfileIntersectionProvider';
 import { ColorSchemeProvider } from '@/contexts/home/layout/ColorSchemeProvider';
 
 export const metadata = {
-  title: 'Daniel NX',
+  title: "Daniel's Portfolio",
   description: "Daniel's Portfolio",
 };
 
@@ -48,8 +53,30 @@ export default function RootLayout({ children }: PropsWithChildren) {
                 {/* <div className='min-h-screen' /> */}
               </div>
             </div>
-            <footer className='h-20 p-5 w-full border-t border-light-border dark:border-dark-border text-base'>
-              Created using NextJS and Tailwind
+            <footer className='flex flex-row justify-center align-middle p-5 w-full border-t dark:border-0 border-light-border bg-light-bg dark:bg-dark-bg-hg-600'>
+              <div className='flex flex-col md:flex-row justify-center items-center w-11/12 xl:w-9/12'>
+                <p className='text-sm sm:text-base lg:text-lg mr-4 mb-4 md:mb-0'>
+                  Built with
+                </p>
+                <div className='grid grid-cols-2 md:grid-cols-4 gap-4 text-xs sm:text-sm text-light-text dark:text-dark-text'>
+                  <div className='flex flex-row items-center px-2 py-1 border border-light-border dark:border-dark-border rounded-md'>
+                    <SiNextJs className='h-3 w-3 sm:h-4 sm:w-4' />
+                    <div className='ml-2'>NextJS</div>
+                  </div>
+                  <div className='flex flex-row items-center px-2 py-1 border border-light-border dark:border-dark-border rounded-md'>
+                    <SiTailwind className='h-3 w-3 sm:h-4 sm:w-4' />
+                    <div className='ml-2'>Tailwind</div>
+                  </div>
+                  <div className='flex flex-row items-center px-2 py-1 border border-light-border dark:border-dark-border rounded-md'>
+                    <SiReact className='h-3 w-3 sm:h-4 sm:w-4' />
+                    <div className='ml-2'>ReactJS</div>
+                  </div>
+                  <div className='flex flex-row items-center px-2 py-1 border border-light-border dark:border-dark-border rounded-md'>
+                    <SiTypescript className='h-3 w-3 sm:h-4 sm:w-4' />
+                    <div className='ml-2'>Typescript</div>
+                  </div>
+                </div>
+              </div>
             </footer>
           </div>
         </ColorSchemeProvider>
