0dddcd5dd82a5628055428034de81ea6bad927b9
Daniel B <danielynx@proton.me>
2023-10-28 21:29:46 -0400

    fix: :bug: adjust the font configuration to avoid error during the app build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,12 +17,15 @@ export const metadata = {
 const sans = Sans({
   subsets: ['latin'],
   display: 'swap',
+  adjustFontFallback: false,
   weight: '300',
   variable: '--font-sans',
 });
 
 const serif = Serif({
   subsets: ['latin'],
+  display: 'swap',
+  adjustFontFallback: false,
   weight: '400',
   variable: '--font-serif',
 });
