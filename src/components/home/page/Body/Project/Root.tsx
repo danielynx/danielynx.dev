2a2da9885a2f132f190ca37772d0259aa446d2bf
Daniel B <danielynx@proton.me>
2023-11-02 19:23:41 -0400

    refactor: :art: implement TailwindVariants in the course and project components

--- src/components/home/page/Body/Project/Root.tsx
+++ src/components/home/page/Body/Project/Root.tsx
@@ -1,12 +1,35 @@
 import { PropsWithChildren } from 'react';
+import { tv } from 'tailwind-variants';
+
 import { SharedTab } from '@/components/shared/Tab';
 
+const tvStyle = tv(
+  {
+    base: 'grid m-2',
+    variants: {
+      size: {
+        initial: 'grid-cols-1 gap-4',
+        sm: 'grid-cols-2',
+        xl: 'gap-5',
+      },
+    },
+    defaultVariants: {
+      size: 'initial',
+    },
+  },
+  {
+    responsiveVariants: ['sm', 'xl'],
+  },
+);
+
 export function ProjectRoot({ children }: PropsWithChildren) {
+  const style = tvStyle({
+    size: { sm: 'sm', xl: 'xl' },
+  });
+
   return (
     <SharedTab.Panel>
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-5 m-2'>
-        {children}
-      </div>
+      <div className={style}>{children}</div>
     </SharedTab.Panel>
   );
 }
