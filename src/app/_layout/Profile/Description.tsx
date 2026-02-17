50a7015986bc9490beb225da22351db3b9cd7735
Daniel Backes <danielynx@proton.me>
2026-02-17 15:29:52 -0400

    fix: add missing period in profile description

--- src/app/_layout/Profile/Description.tsx
+++ src/app/_layout/Profile/Description.tsx
@@ -2,7 +2,7 @@ export function Description() {
     return (
         <div className="mt-2 text-base font-light">
             Computing Scientist and Full-Stack Developer working with PHP and
-            JavaScript. Currently studying Next.js and Generative AI
+            JavaScript. Currently studying Next.js and Generative AI.
         </div>
     );
 }
