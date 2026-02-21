b60fcedcc7a63a3cadb6451f33c9e350d2ddff96
Daniel Backes <danielynx@proton.me>
2026-02-21 17:08:03 -0400

    style: centralize mongodb screenshot

--- src/app/@children/courses/mongodb/page.tsx
+++ src/app/@children/courses/mongodb/page.tsx
@@ -1,21 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import { CgScreenShot } from "react-icons/cg";
-import { tv } from "tailwind-variants";
 import { SessionHeader } from "@/component/SessionHeader";
 
-const tvStyle = tv({
-    slots: {
-        container: "space-y-4 leading-relaxed text-base lg:text-lg",
-    },
-});
-
 export default function Page() {
-    const style = tvStyle();
-
     return (
         <div>
-            <div className={style.container()}>
+            <div className="space-y-4 leading-relaxed text-base lg:text-lg">
                 <Link
                     className="underline decoration-1 hover:decoration-2 underline-offset-2"
                     href="https://learn.mongodb.com/"
@@ -27,12 +18,14 @@ export default function Page() {
                 hands-on labs to build skills in MongoDB development and administration.
             </div>
             <SessionHeader Icon={CgScreenShot} title="Screenshots" />
-            <Image
-                src="/courses/mongodb/proof-of-completion.webp"
-                width={800}
-                height={400}
-                alt="Proof of completion of MongoDB University course"
-            />
+            <div className="flex flex-col items-center">
+                <Image
+                    src="/courses/mongodb/proof-of-completion.webp"
+                    width={800}
+                    height={400}
+                    alt="Proof of completion of MongoDB University course"
+                />
+            </div>
         </div>
     );
 }
